db84d41c2caead81043198451cbf376d96624dc1
Adlet <eypk777@gmail.com>
2022-05-01 00:03:12 +0900

    trigger, collide 분리, z축 회전 품

--- Assets/Scripts/TriggerObject.cs
+++ Assets/Scripts/TriggerObject.cs
@@ -2,41 +2,51 @@ using UnityEngine;
 
 public class TriggerObject : MonoBehaviour
 {
+    [SerializeField] private bool isTrigger;
     [SerializeField] private bool directionFixed;
     [SerializeField] private Vector2 direction;
     [SerializeField] private float power;
     [SerializeField] private float stunTime;
     private MainCharacter triggerObject;
     private void OnTriggerEnter2D(Collider2D other) {
-        if (other.tag == "Player"){
-            Debug.Log("Player Collided");
-            this.triggerObject = other.GetComponent<MainCharacter>();
-            if (directionFixed)
-                triggerObject.CollidedWithTrigger(direction, power, stunTime);
-            else{
-                float x = triggerObject.transform.localPosition.x - this.transform.localPosition.x;
-                float y = triggerObject.transform.localPosition.y - this.transform.localPosition.y;
-                direction.x = x;
-                direction.y = y;
-                triggerObject.CollidedWithTrigger(direction, power, stunTime);
-            }
-            return;
+        if (!isTrigger) return;
+        if (other.tag != "Player") return;
+
+        this.triggerObject = other.GetComponent<MainCharacter>();
+        if (directionFixed)
+            triggerObject.CollidedWithTrigger(direction, power, stunTime);
+        else
+        {
+            float x = triggerObject.transform.localPosition.x - this.transform.localPosition.x;
+            float y = triggerObject.transform.localPosition.y - this.transform.localPosition.y;
+            direction.x = x;
+            direction.y = y;
+            triggerObject.CollidedWithTrigger(direction, power, stunTime);
         }
     }
 
     private void OnTriggerStay2D(Collider2D other) {
-        if (other.tag == "Player"){
-            Debug.Log("Player Collided");
-            if (directionFixed)
-                triggerObject.CollidedWithTrigger(direction, power / 2, stunTime);
-            else{
-                float x = triggerObject.transform.localPosition.x - this.transform.localPosition.x;
-                float y = triggerObject.transform.localPosition.y - this.transform.localPosition.y;
-                direction.x = x;
-                direction.y = y;
-                triggerObject.CollidedWithTrigger(direction, power / 2, stunTime);
-            }
-            return;
+        if (!isTrigger) return;
+        if (other.tag != "Player") return;
+
+        if (directionFixed)
+            triggerObject.CollidedWithTrigger(direction, power / 2, stunTime);
+        else
+        {
+            float x = triggerObject.transform.localPosition.x - this.transform.localPosition.x;
+            float y = triggerObject.transform.localPosition.y - this.transform.localPosition.y;
+            direction.x = x;
+            direction.y = y;
+            triggerObject.CollidedWithTrigger(direction, power / 2, stunTime);
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if (isTrigger) return;
+
+        this.triggerObject = other.collider.GetComponent<MainCharacter>();
+
+        var relVelocity = other.GetContact(0).relativeVelocity;
+        triggerObject.CollidedWithTrigger(-relVelocity, power, stunTime);
     }
 }
