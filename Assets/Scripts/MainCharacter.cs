e2136550211f4abb8321666f944ec2ec6a86e152
jaewoo.kim <jaewoo986@gmail.com>
2022-04-30 14:57:41 +0900

    Add basic move action.

---
+++ Assets/Scripts/MainCharacter.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class MainCharacter : MonoBehaviour
+{
+    [SerializeField] private float _jumpImpulseSize = 1f;
+
+    private Rigidbody2D _rigidbody;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    public void JumpToward(Vector2 targetPosition)
+    {
+        var dir = targetPosition - (Vector2)transform.position;
+        
+        dir = dir.normalized * _jumpImpulseSize;
+        
+        _rigidbody.AddForce(dir, ForceMode2D.Impulse);
+    }
+}
---
+++ Assets/Scripts/MainInput.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+
+public class MainInput : MonoBehaviour
+{
+    [SerializeField] private MainCharacter _mainCharacter;
+
+
+    private Camera _targetCamera;
+
+    private void Start()
+    {
+        _targetCamera = Camera.main;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _mainCharacter.JumpToward(GetMouseWorldPosition());
+        }
+    }
+
+    private Vector2 GetMouseWorldPosition()
+    {
+        return (Vector2)_targetCamera.ScreenToWorldPoint(Input.mousePosition);
+    }
+}
