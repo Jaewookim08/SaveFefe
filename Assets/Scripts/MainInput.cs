3d9419934d9fd060e22c057d57943d82e6461391
jaewoo.kim <jaewoo986@gmail.com>
2022-04-30 16:48:20 +0900

    Input 한 프레임 여러 키 씹히는 문제 수정 & 점프 키 ASDF으로 설정.

--- Assets/Scripts/MainInput.cs
+++ Assets/Scripts/MainInput.cs
@@ -6,7 +6,9 @@ public class MainInput : MonoBehaviour
     [SerializeField] private MainCharacter _mainCharacter;
 
 
+    private static readonly KeyCode[] JumpKeys = { KeyCode.A, KeyCode.S, KeyCode.D, KeyCode.F };
     private Camera _targetCamera;
+
 
     private void Start()
     {
@@ -15,10 +17,18 @@ public class MainInput : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0) || Input.anyKeyDown )
+        if (Input.GetMouseButtonDown(0))
         {
             _mainCharacter.JumpToward(GetMouseWorldPosition());
         }
+        foreach (var key in JumpKeys)
+        {
+            if (Input.GetKeyDown(key))
+            {
+                _mainCharacter.JumpToward(GetMouseWorldPosition());
+            }
+        }
+        
     }
 
     private Vector2 GetMouseWorldPosition()
