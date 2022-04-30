6acbd4055fe85ffec6f439e40c2384aee6e1c252
jaewoo.kim <jaewoo986@gmail.com>
2022-05-01 03:43:41 +0900

    방향돌리기

---
+++ Assets/Scripts/FlipAccordingToDirection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class FlipAccordingToDirection : MonoBehaviour
+{
+    private Vector3 _lastPos;
+    private SpriteRenderer _spriteRenderer;
+
+    private void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void FixedUpdate()
+    {
+        var pos = transform.position;
+
+        if ((pos - _lastPos).sqrMagnitude < 0.001) return;
+
+        _spriteRenderer.flipX = pos.x > _lastPos.x;
+
+        _lastPos = transform.position;
+    }
+}
