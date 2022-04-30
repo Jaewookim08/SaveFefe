fb8d0ac914e07ca4f7b70aa5d1681697442386a1
jaewoo.kim <jaewoo986@gmail.com>
2022-04-30 19:07:57 +0900

    눈동자 마우스 따라가기 추가.

---
+++ Assets/Scripts/MouseFollowingEye.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class MouseFollowingEye : MonoBehaviour
+{
+    [SerializeField] private Transform _watchFromPoint;
+    [SerializeField] private float _eyeRadius;
+    [SerializeField] private float _eyeMouseRatio;
+    [SerializeField] private Transform[] _pupils;
+
+    private Camera _targetCamera;
+
+
+    private void Start()
+    {
+        _targetCamera = Camera.main;
+    }
+
+    private void Update()
+    {
+        LookAt(GetMouseWorldPosition() - (Vector2)_watchFromPoint.position);
+    }
+
+    private void LookAt(Vector2 at)
+    {
+        at /= _eyeMouseRatio;
+        var pupilPos = at.normalized * Math.Min(at.magnitude, _eyeRadius);
+
+        foreach (var pupil in _pupils)
+        {
+            pupil.localPosition = pupilPos;
+        }
+        
+    }
+
+    private Vector2 GetMouseWorldPosition()
+    {
+        return (Vector2)_targetCamera.ScreenToWorldPoint(Input.mousePosition);
+    }
+}
