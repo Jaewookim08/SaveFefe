1fe9d16bb4ef1b804e7ae3b5c6bea5b6737dceab
jaewoo.kim <jaewoo986@gmail.com>
2022-05-01 18:31:01 +0900

    Add title scene.

---
+++ Assets/Scripts/TitleSceneManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleSceneManager : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            SceneManager.LoadScene("MainScene");
+        }
+    }
+}
